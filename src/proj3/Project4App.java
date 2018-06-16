7ef781e979e145cfe3e5c668ffe7ca66a3d9b7c9
Daniel Choi <danielchoi@Daniels-MBP-2.fios-router.home>
2018-06-16 05:15:27 -0400

    Initial commit

---
+++ src/proj3/Project4App.java
@@ -0,0 +1,89 @@
+package proj3;
+
+import javax.swing.JOptionPane;
+
+/**
+ * <p> Title: Project #4: Math Quiz, Part 2 </p>
+ *
+ * <p> Description: this program generates random addition or subtraction
+ * questions. After user inputs answer, the program will determine the answer
+ * and let the user know the answer was correct or not. when the program ends,
+ * it prompts result. </p>
+ *
+ * <p> Due 18 June 18 11:59 pm </p>
+ *
+ * @author Hamin Choi Student (N00765091@students.ncc.edu)
+ */
+
+public class Project4App {
+    public static void main(String[] args) {
+        //number of problems
+        int num_problems = 15;
+
+        //stores user inputs
+        String[] userInputs = new String[25];
+
+        //stores converted answers
+        int[] answer = new int[25];
+        int addCorrect = 0; //  number of correct answers from add problems
+        int addIncorrect = 0;   //  number of incorrect answers from add problems
+        int subCorrect = 0; //  number of correct answers from sub problems
+        int subIncorrect = 0;   //  number of incorrect answers from sub problems
+        double percentage = 0;  //  percent of correct answers
+        double percentOutput = 0;   //  percent with double decimal places
+
+
+        //for loop
+        for (int i = 0; i < num_problems; i++) {
+            Question theQuestion = new Question();
+
+            //  it shows input dialog and stores user inputs
+            userInputs[i] = JOptionPane.showInputDialog("Question #" + (i+1)
+                    + ":" + "\n" + "What is the answer?" + "\n"
+                    + theQuestion.toString() + " = ", 0);
+
+            //  it converts String to integer to determine the answer is correct
+            answer[i] = Integer.parseInt(userInputs[i]);
+
+            //  this if statement determine the answer
+            if (answer[i] == theQuestion.determineAnswer()  //  if add answer is correct
+                    && (theQuestion.determineOperator()))	{
+                addCorrect += 1;    //  add 1
+                JOptionPane.showMessageDialog(null, "Congratulations! You got it correct!");
+            }
+            else if (answer[i] == theQuestion.determineAnswer() //  if sub answer is correct
+                    && (theQuestion.determineOperator() == false))	{
+                subCorrect += 1;
+                JOptionPane.showMessageDialog(null, "Congratulations! You got it correct!");
+            }
+            else if	(answer[i] != theQuestion.determineAnswer()
+                    && theQuestion.determineOperator())	{
+                addIncorrect += 1;
+                JOptionPane.showMessageDialog(null, "Sorry, "
+                        + theQuestion.toString() + " = " + theQuestion.determineAnswer());
+            }
+            else {
+                subIncorrect += 1;
+                JOptionPane.showMessageDialog(null, "Sorry, "
+                        + theQuestion.toString() + " = " + theQuestion.determineAnswer());
+            }
+
+            //  calculate correct percentage
+            percentage = 100.0 * (addCorrect + subCorrect) / (addCorrect + addIncorrect + subCorrect + subIncorrect);
+            percentOutput = Double.parseDouble(String.format("%.2f", percentage));
+
+            //  FOR EXTRA CREDIT
+	//		if (addCorrect + subCorrect == 10 && i == 9)
+	//			break;
+	//		else if (percentage >= 80 && i >= 10)
+	//			break;
+	//		else num_problems = 25;
+        }
+
+        //  displays output
+        JOptionPane.showMessageDialog(null, "Addition:" + "\n" + "You got "
+                + addCorrect + " correct, and " + addIncorrect + " incorrect."+ "\n" + "Subtraction:" + "\n"
+                + "You got " + subCorrect + " correct, and " + subIncorrect + " incorrect." + "\n"
+                + "The percent correct is " + percentOutput + ".");
+    }
+}
---
+++ src/proj3/Question.java
@@ -0,0 +1,93 @@
+package proj3;
+
+import java.util.Random;
+
+/**
+ * <p>Title: The Question class</p>
+ *
+ * <p>Description: This class will represent a single math problem
+ * that can be addition or subtraction. Operands will be also randomly
+ * generated within given range. It also provides the answer of the problem
+ * </p>
+ *
+ * @author Hamin Choi Student (N00765091@students.ncc.edu)
+ */
+
+public class Question {
+    //instance variables
+    private int firstOperand = 0;
+    private int secondOperand = 0;
+    private int operator;
+    private int[] answer = new int[25];
+
+    /**
+     * Question method -- default constructor. it generates random integer number
+     * within given range to determine operands and operator.
+     */
+
+    public Question() {
+        Random random = new Random();
+        operator = random.nextInt(2); //decide the operator of the problem
+        if(operator == 0) { // if operator = 0, question type = addition
+            firstOperand = random.nextInt(12) + 1; // 0 =< firstOperand =< 12
+            secondOperand = random.nextInt(12) + 1; // 0 =< secondOperand =< 12
+        }
+        else
+            if (operator == 1) { // if operator = 1, question type = subtraction
+            firstOperand = random.nextInt(7) + 6; // 6 =< firstOperand =< 12
+            secondOperand = random.nextInt(firstOperand); // 0 =< secondOperand < firstOperand
+        }
+    }
+
+    /**
+     * toString method -- this method returns the state of the question object
+     * @return a reference to a String object that contains the value of
+     * firstOperand, secondOperand and operator
+     */
+
+    public String toString() {
+        String returnMe = null;
+        if(operator == 0) { // if the question type is addition
+            returnMe = (firstOperand + " + " + secondOperand);
+        }
+        else
+            if(operator == 1) { // if the question type is subtraction
+                returnMe = (firstOperand + " - " + secondOperand);
+            }
+        return returnMe;
+    }
+
+    /**
+     * determineAnswer method -- this method will calculate random operands
+     * based on the operator decided
+     * @return the sum of two operands
+     */
+
+    public int determineAnswer() {
+        int returnMe = 0;
+        if(operator == 0) { // if the question type is addition
+            returnMe = (firstOperand + secondOperand);
+        }
+        else
+            if(operator == 1) { // if the question type is subtraction
+                returnMe = (firstOperand - secondOperand);
+            }
+        return returnMe;
+    }
+
+    /**
+     * determineOperator method -- this method will determine operator
+     * whether addition or subtraction
+     * @return
+     */
+
+    public boolean determineOperator() {
+        boolean returnMe = false;
+        if(operator == 0) // if the question type is addition
+            returnMe = true;
+        else
+        if(operator == 1) // if the question type is subtraction
+            returnMe = false;
+        return returnMe;
+    }
+}
